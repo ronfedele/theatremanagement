814f6553a95f7550469d0a7f4c738a8983800cc9
ronfedele <ronfedele@jazzyjelly.com>
2026-03-20 14:35:18 -0700

    Delete src directory

--- src/app/page.tsx
+++
@@ -1,7 +0,0 @@
-import { redirect } from 'next/navigation'
-
-// Root page just sends everyone to dashboard
-// AppShell handles auth check client-side
-export default function RootPage() {
-  redirect('/dashboard')
-}
